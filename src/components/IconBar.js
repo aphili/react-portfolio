7c5e37f837e035c363670bdc5ae92e9d2daa99c8
aphili <andrew@philippick.com>
2019-03-18 14:54:36 +0100

    build refractoring

--- src/components/IconBar.js
+++ src/components/IconBar.js
@@ -10,7 +10,7 @@ export default function IconBar(){
                     <a href="https://github.com/aphili/" target="_blank"><Icon size="large" link name="github"/></a>
                     <a href="https://twitter.com/iaphili" target="_blank"><Icon size="large" link name="twitter"/></a>
                     <a href="mailto:andrew@philippick.com" target="_blank"><Icon size="large" link name="mail"/></a>
-                    <a href="../../public/curriculum-vitae.pdf" target="_blank"><Icon size="large" link name="file text"/></a>
+                    <a href="%PUBLIC_URL%/curriculum-vitae.pdf" target="_blank"><Icon size="large" link name="file text"/></a>
                 </Responsive>
             </Container>
         </div>
